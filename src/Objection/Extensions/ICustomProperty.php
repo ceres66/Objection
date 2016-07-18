7f2d060969287fb8721e9b9392d8259cbe3ea22b
Alexey Puchkov <alexey@oktopost.com>
2016-07-18 16:57:44 +0300

    Fixes in ICustomProperty

--- src/Objection/Extensions/ICustomProperty.php
+++ src/Objection/Extensions/ICustomProperty.php
@@ -7,20 +7,26 @@ use Objection\LiteObject;
 
 interface ICustomProperty
 {
+	/**
+	 * @return static
+	 */
 	public function __clone();
 	
 	/**
+	 * @return bool If false, a new instance will be created for each field in each object. 
+	 */
+	public function isStateless();
+	
+	/**
 	 * @param LiteObject $object
-	 */
-	public function bind(LiteObject $object);
-	
-	/**
 	 * @param mixed $value
-	 */
-	public function set($value);
-	
-	/**
 	 * @return mixed
 	 */
-	public function get();
+	public function set(LiteObject $object, $value);
+	
+	/**
+	 * @param LiteObject $object
+	 * @return mixed
+	 */
+	public function get(LiteObject $object);
 }
