9af1fe50cfe852ce737a29b0bcd0c48b6bebb64d
Alexey Puchkov <alexey@oktopost.com>
2016-09-26 09:41:25 +0300

    Add set method to ValuesProcessorCreator

--- src/Objection/Mapper/Utils/ValuesProcessorCreator.php
+++ src/Objection/Mapper/Utils/ValuesProcessorCreator.php
@@ -3,6 +3,7 @@ namespace Objection\Mapper\Utils;
 
 
 use Objection\Mapper\Base\Values\IObjectValuesProcessor;
+use Objection\Mapper\Base\Values\IValueProcessor;
 use Objection\Mapper\ValuesProcessor;
 
 
@@ -41,6 +42,17 @@ class ValuesProcessorCreator
 	public function callback($field, $toObject, $toRawData)
 	{
 		$this->objectValuesProcessor->add($field, new ValuesProcessor\CallbackValueProcessor($toObject, $toRawData));
+		return $this;
+	}
+	
+	/**
+	 * @param string $field
+	 * @param IValueProcessor $processor
+	 * @return static
+	 */
+	public function set($field, IValueProcessor $processor)
+	{
+		$this->objectValuesProcessor->add($field, $processor);
 		return $this;
 	}
 }
