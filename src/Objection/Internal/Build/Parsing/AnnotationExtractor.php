c6f9b4800993ba8f9c69a4927793b72d1ad5b94f
Alexey Puchkov <alexey@oktopost.com>
2016-10-29 09:32:22 +0300

    Annotation extractor will extract properties from class

--- src/Objection/Internal/Build/Parsing/AnnotationExtractor.php
+++ src/Objection/Internal/Build/Parsing/AnnotationExtractor.php
@@ -11,15 +11,71 @@ class AnnotationExtractor
 
 
 	/**
-	 * @param \ReflectionClass|\ReflectionMethod|\ReflectionProperty $source
+	 * @param string $name
+	 * @return string[][]
+	 */
+	private static function extract($name, $doc)
+	{
+		$result = [];
+		$regex = "/^[ \\t]*\\*[ \\t]*@{$name}[ \\t]+(.*)$/mi";
+		preg_match_all($regex, $doc, $matches);
+		
+		foreach ($matches[1] as $match)
+		{
+			if (!$match) continue;
+			
+			$matchedString = str_replace("\t", ' ', $match);
+			$description = array_filter(explode(' ', $matchedString));
+			
+			if (!$description) continue;
+			
+			$result[] = $description;
+		}
+		
+		return $result;
+	}
+
+	/**
+	 * @param \ReflectionClass $source
+	 * @param array $description
+	 * @return PropertyAnnotation
+	 */
+	private static function createPropertyAnnotation(\ReflectionClass $source, array $description)
+	{
+		if ($description[0][0] == '$' || count($description) == 1)
+		{
+			$types = [];
+			$name = $description[0];
+		}
+		else 
+		{
+			$types = explode('|', $description[0]);
+			$name = $description[1];
+		}
+		
+		if ($name[0] == '$')
+		{
+			$name = substr($name, 1);
+		}
+		
+		return new PropertyAnnotation($source, $name, $types);
+	}
+	
+
+	/**
+	 * @param \ReflectionClass $source
 	 * @return PropertyAnnotation[]
 	 */
-	public static function getProperties($source)
+	public static function getProperties(\ReflectionClass $source)
 	{
 		$comment = $source->getDocComment();
+		$result = [];
 		
-		$regex = "/^[ \\t]*\\*[ \\t]*@var ([\\w\\\\]+)[ \\t]+\\$?{$parameterName}.*$/m";
+		foreach (self::extract('property', $comment) as $item)
+		{
+			$result[] = self::createPropertyAnnotation($source, $item);
+		}
 		
-		return [];
+		return $result;
 	}
 }
--- test/Objection/Internal/Build/Parsing/AnnotationExtractorTest.php
+++ test/Objection/Internal/Build/Parsing/AnnotationExtractorTest.php
@@ -3,6 +3,8 @@ namespace Objection\Internal\Build\Parsing;
 
 
 
+use Objection\Internal\Build\Annotations\PropertyAnnotation;
+
 class EmptyClass
 {
 	private $empty;
@@ -22,6 +24,65 @@ class SinglePropertyClass
 	public static $prop_types = ['mixed'];
 }
 
+/**
+ * @property $prop
+ */
+class PropertyTypesMissing
+{
+	public static $prop_name = 'prop';
+	public static $prop_types = [];
+}
+
+/**
+ * @ProPerty $prop
+ */
+class AnnotationCaseDifferent
+{
+	public static $prop_name = 'prop';
+	public static $prop_types = [];
+}
+
+/**
+ * @property int prop
+ */
+class PropertyNameMissingDollarMissing
+{
+	public static $prop_name = 'prop';
+}
+
+/**
+ * @property prop
+ */
+class NoTypesAndNoDollarSign
+{
+	public static $prop_name = 'prop';
+}
+
+/**
+ * @property 
+ */
+class PropertyDescriptionMissing {}
+
+/**
+ * @property int|string|* $prop
+ */
+class NumberOfPropertyTypes 
+{
+	public static $prop_types = ['int', 'string', '*'];
+}
+
+/**
+ * @property string $prop1
+ * @property int $prop2
+ */
+class NumberOfProperties 
+{
+	public static $prop_1_types = ['string'];
+	public static $prop_1_name = 'prop1';
+	public static $prop_2_types = ['int'];
+	public static $prop_2_name = 'prop2';
+}
+
 
 class AnnotationExtractorTest extends \PHPUnit_Framework_TestCase
 {
@@ -35,5 +96,91 @@ class AnnotationExtractorTest extends \PHPUnit_Framework_TestCase
 	{
 		$result = AnnotationExtractor::getProperties(new \ReflectionClass(SinglePropertyClass::class));
 		$this->assertCount(1, $result);
+	}
+	
+	public function test_getProperties_AnnotationCaseIgnored()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(AnnotationCaseDifferent::class));
+		$this->assertCount(1, $result);
+	}
+	
+	public function test_getProperties_PropertiesExist_ArrayContainsPropertyAnnotationObjects()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(SinglePropertyClass::class));
+		$this->assertInstanceOf(PropertyAnnotation::class, $result[0]);
+	}
+	
+	public function test_getProperties_SourceClassPassedToPropertyAnnotationObject()
+	{
+		$source = new \ReflectionClass(SinglePropertyClass::class);
+		$result = AnnotationExtractor::getProperties($source);
+		$this->assertSame($source, $result[0]->getSourceClass());
+	}
+	
+	public function test_getProperties_PropertyNameResolved()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(SinglePropertyClass::class));
+		$this->assertSame(SinglePropertyClass::$prop_name, $result[0]->getName());
+	}
+	
+	public function test_getProperties_PropertyTypeResolved()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(SinglePropertyClass::class));
+		$this->assertSame(SinglePropertyClass::$prop_types, $result[0]->getTypes());
+	}
+	
+	public function test_getProperties_PropertyTypesMissing_PropertyTypesAreSetToEmptyArray()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(PropertyTypesMissing::class));
+		$this->assertSame(PropertyTypesMissing::$prop_types, $result[0]->getTypes());
+	}
+	
+	public function test_getProperties_PropertyTypesMissing_PropertyNameResolved()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(PropertyTypesMissing::class));
+		$this->assertSame(PropertyTypesMissing::$prop_name, $result[0]->getName());
+	}
+	
+	public function test_getProperties_PropertyNameMissingDollarWithTypesPresent_PropertyNameResolved()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(PropertyNameMissingDollarMissing::class));
+		$this->assertSame(PropertyNameMissingDollarMissing::$prop_name, $result[0]->getName());
+	}
+	
+	public function test_getProperties_TypesAndDollarSignMissing_PropertyNameResolved()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(NoTypesAndNoDollarSign::class));
+		$this->assertSame(NoTypesAndNoDollarSign::$prop_name, $result[0]->getName());
+	}
+	
+	public function test_getProperties_PropertyDescriptionMissing_PropertyIgnored()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(PropertyDescriptionMissing::class));
+		$this->assertSame([], $result);
+	}
+	
+	public function test_getProperties_NumberOfPropertyTypes_AllTypesResolved()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(NumberOfPropertyTypes::class));
+		$this->assertSame(NumberOfPropertyTypes::$prop_types, $result[0]->getTypes());
+	}
+	
+	public function test_getProperties_NumberOfProperties_AllPropertiesLoaded()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(NumberOfProperties::class));
+		$this->assertCount(2, $result);
+	}
+	
+	public function test_getProperties_NumberOfProperties_AllPropertiesCorrect()
+	{
+		$result = AnnotationExtractor::getProperties(new \ReflectionClass(NumberOfProperties::class));
+		
+		$this->assertInstanceOf(PropertyAnnotation::class, $result[0]);
+		$this->assertSame(NumberOfProperties::$prop_1_name, $result[0]->getName());
+		$this->assertSame(NumberOfProperties::$prop_1_types, $result[0]->getTypes());
+		
+		$this->assertInstanceOf(PropertyAnnotation::class, $result[1]);
+		$this->assertSame(NumberOfProperties::$prop_2_name, $result[1]->getName());
+		$this->assertSame(NumberOfProperties::$prop_2_types, $result[1]->getTypes());
 	}
 }
