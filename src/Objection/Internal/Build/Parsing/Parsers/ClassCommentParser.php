55e95ccbec1f036238f6145e5dc9b1e6fcbc1ccb
Alexey Puchkov <alexey@oktopost.com>
2016-11-09 07:39:44 +0200

    Add partial ClassCommentParser

--- src/Objection/Internal/Build/Parsing/Parsers/ClassCommentParser.php
+++ src/Objection/Internal/Build/Parsing/Parsers/ClassCommentParser.php
@@ -3,12 +3,23 @@ namespace Objection\Internal\Build\Parsing\Parsers;
 
 
 use Objection\Internal\Base\Parsing\AbstractPropertyParser;
+use Objection\Internal\Build\Parsing\AnnotationExtractor;
 
 
 class ClassCommentParser extends AbstractPropertyParser
 {
 	public function parse()
 	{
+		$propertyAnnotations = AnnotationExtractor::getProperties($this->getClass());
 		
+		foreach ($propertyAnnotations as $annotation)
+		{
+			$property = $this->getOrCreateProperty($annotation->getName());
+			
+			foreach ($annotation->getTypes() as $type)
+			{
+				
+			}
+		}
 	}
 }
